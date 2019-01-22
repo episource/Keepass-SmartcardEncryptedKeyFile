f140f6564a5138a5e5c24cf768fb31afe40d5114
Philipp Serr <episource@gmx.de>
2019-01-22 18:37:27 +0100

    Prevent PIN dialog on certificate listing

--- SmartcardEncryptedKeyFile/Crypto/RSACryptoServiceProviderKeyPair.cs
+++ SmartcardEncryptedKeyFile/Crypto/RSACryptoServiceProviderKeyPair.cs
@@ -2,6 +2,8 @@ using System;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
+using Episource.KeePass.EKF.Crypto.Windows;
+
 namespace Episource.KeePass.EKF.Crypto {
     /// <summary>
     /// Gives access to RSA smart cards that are compatible with the windows crypto service provider framework.
@@ -34,7 +36,7 @@ namespace Episource.KeePass.EKF.Crypto {
                 throw new ArgumentNullException(paramName: "cert");
             }
 
-            var isRsa = (cert.PrivateKey ?? cert.PublicKey.Key) is RSA;
+            var isRsa = cert.PublicKey.Key is RSA;
             if (!isRsa) {
                 if (nullOnError) {
                     return null;
@@ -42,16 +44,26 @@ namespace Episource.KeePass.EKF.Crypto {
                 throw new ArgumentException(message: "Not an RSA based certificate.", paramName: "cert");
             }
 
-            var cspAlgorithm = (cert.PrivateKey ?? cert.PublicKey.Key) as ICspAsymmetricAlgorithm;
-            if (cspAlgorithm == null) {
+
+            // Important: Try to avoid accessing cert.PrivateKey to retrieve cspInfo if possible!
+            // Accessing cert.PrivateKey requests access to the private key itself, asking the user for its pin!
+            try {
+                var cspParams = NativeCapi.GetParameters(cert);
+                var cspInfo = new CspKeyContainerInfo(cspParams);
+                return new RSACryptoServiceProviderKeyPair(cert, cspInfo);
+            } catch (DllNotFoundException) {
+                // this is likely to cause a pin prompt!
+                var cspAlgorithm = cert.PrivateKey as ICspAsymmetricAlgorithm;
+                if (cspAlgorithm != null) {
+                    return new RSACryptoServiceProviderKeyPair(cert, cspAlgorithm.CspKeyContainerInfo);
+                }
                 if (nullOnError) {
                     return null;
                 }
+                
                 throw new ArgumentException(message: "Certificate not backed by windows crypto service provider.",
                     paramName: "cert");
             }
-            
-            return new RSACryptoServiceProviderKeyPair(cert, cspAlgorithm.CspKeyContainerInfo);
         }
 
         public bool IsSmartcard {
