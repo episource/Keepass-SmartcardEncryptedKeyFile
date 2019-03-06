b4ced26258d49815a15780b4c77b29b84163e4ea
Philipp Serr <episource@gmx.de>
2019-03-06 23:20:22 +0100

    Support credential UI on secure desktop

--- SmartcardEncryptedKeyFile/UI/SmartcardRequiredDialog.cs
+++ SmartcardEncryptedKeyFile/UI/SmartcardRequiredDialog.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 using episource.unblocker;
 using episource.unblocker.hosting;
@@ -15,6 +18,74 @@ using Episource.KeePass.EKF.Util;
 
 namespace Episource.KeePass.EKF.UI {
     public sealed class SmartcardRequiredDialog : Form {
+        
+        [Serializable]
+        private class WorkerResult {
+
+            private readonly LinkedList<Exception> exceptions = new LinkedList<Exception>();
+            
+            private object result;
+            
+            // ReSharper disable once MemberHidesStaticFromOuterClass
+            private IEnumerable<IntPtr> remainingDesktopHandles;
+            
+            public IEnumerable<IntPtr> RemainingDesktopHandles {
+                get {
+                    if (this.remainingDesktopHandles == null) {
+                        throw new InvalidOperationException("remainingDesktopHandles not set");
+                    }
+                    return this.remainingDesktopHandles;
+                }
+            }
+
+            public object GetResultOrThrow() {
+                if (this.exceptions.Count > 1) {
+                    throw new AggregateException("Smartcard operation failed.", this.exceptions);
+                } 
+                if (this.exceptions.Count == 1) {
+                    ExceptionDispatchInfo.Capture(this.exceptions.First()).Throw();
+                    throw new InvalidOperationException("This code should be unreachable...");
+                }
+
+                return this.result;
+            }
+
+            public void AddException(Exception e) {
+                this.exceptions.AddLast(e);
+            }
+
+            // ReSharper disable once ParameterHidesMember
+            public void SetResult(object result) {
+                this.result = result;
+            }
+
+            // ReSharper disable once ParameterHidesMember
+            public void SetRemainingDesktopHandles(IEnumerable<IntPtr> remainingDesktopHandles) {
+                if (this.remainingDesktopHandles != null) {
+                    throw new InvalidOperationException("remainingDesktopHandles already set");
+                }
+                
+                this.remainingDesktopHandles = remainingDesktopHandles;
+            }
+            
+        }
+        
+        private const string DefaultDesktopName = "Default";
+        
+        // A dedicated worker process pool is used:
+        // - smartcard operations involve native code without support for cancellation; hence the process needs
+        // to be killed to cancel
+        // - Some smartcards require the pin to be entered just once. This is bound to the requesting process. Hence
+        // the standby timeout is chosen quite long.
+        // - Worker limit is set to one, such that smartcard operations are not done in parallel.
+        // - Unblocker creates the first worker process on first invocation only, therefore there's no need for
+        // explicit lazy initialization
+        private static Unblocker smartcardWorker = new Unblocker(
+            standbyDelay: TimeSpan.FromSeconds(500000), maxWorkers: 1, debug: DebugMode.None);
+
+        // set via ReplaceRemainingHandles only
+        private static volatile ISet<IntPtr> remainingDesktopHandles = new HashSet<IntPtr>();
+        
         private readonly TableLayoutPanel layout = new TableLayoutPanel();
         private readonly CancellationTokenSource cts;
 
@@ -138,48 +209,165 @@ namespace Episource.KeePass.EKF.UI {
             Expression<Action<CancellationToken>> cryptoOperation, CancellationToken ct = default(CancellationToken),
             ForcedCancellationMode forcedCancellationMode = ForcedCancellationMode.KillImmediately
         ) {
-            await DoCryptoImpl(ct,
-                (u, realCt, pRef) =>
-                    u.InvokeAsync(cryptoOperation, realCt, forcedCancellationMode: forcedCancellationMode,
-                        workerProcessRef: pRef).AddDefaultResult<object>()
-            ).ConfigureAwait(false);
+            await DoCryptoImpl(InvocationRequest.FromExpression(cryptoOperation), ct, forcedCancellationMode)
+                .ConfigureAwait(false);
         }
         
         public static async Task<T> DoCryptoAsync<T>(
             Expression<Func<CancellationToken, T>> cryptoOperation, CancellationToken ct = default(CancellationToken),
             ForcedCancellationMode forcedCancellationMode = ForcedCancellationMode.KillImmediately
         ) {
-            return await DoCryptoImpl(ct,
-                (u, realCt, pRef) =>
-                    u.InvokeAsync(cryptoOperation, realCt, forcedCancellationMode: forcedCancellationMode,
-                        workerProcessRef: pRef)
-            ).ConfigureAwait(false);
+            return (T) await DoCryptoImpl(InvocationRequest.FromExpression(cryptoOperation), ct, forcedCancellationMode)
+                .ConfigureAwait(false);
         }
 
-        private static async Task<T> DoCryptoImpl<T>(
-            CancellationToken ct, Func<Unblocker, CancellationToken, WorkerProcessRef, Task<T>> cryptoInvocation
+        private static async Task<object> DoCryptoImpl(
+            InvocationRequest cryptoOperationRequest, CancellationToken ct,
+            ForcedCancellationMode forcedCancellationMode
         ) {
             var cts = CancellationTokenSource.CreateLinkedTokenSource(ct);
             var pRef = new WorkerProcessRef();
 
             var activeForm = NativeForms.GetActiveWindow();
-
+            
             var scRequiredDialog = new SmartcardRequiredDialog(activeForm, cts);
             scRequiredDialog.Show(activeForm);
 
             try {
-                var cryptoTask = cryptoInvocation(UnblockerSingleton.DefaultInstance, cts.Token, pRef);
+                Expression<Func<CancellationToken, WorkerResult>> desktopBoundInvocation = cct => SetDesktopAndExecute(
+                    cct, cryptoOperationRequest.ToPortableInvocationRequest(), NativeForms.GetCurrentThreadDesktopName(),
+                    GetAndResetRemainingHandles());
+                
+                var cryptoTask = smartcardWorker.InvokeAsync(
+                    desktopBoundInvocation, cts.Token, forcedCancellationMode: forcedCancellationMode,
+                    workerProcessRef: pRef);
                 using (var cryptoProcessWinEvents = new NativeWinEvents(pRef.WorkerProcess)) {
                     cryptoProcessWinEvents.ForegroundChanged +=
                         (sender, args) => NativeForms.SetOwner(args.EventSource, activeForm);
-
-                    return await cryptoTask;
+                    
+                    // continueOnCapturedContext: true => finally must run within UI thread!
+                    var retVal = await cryptoTask.ConfigureAwait(true);
+                    AddRemainingHandles(retVal.RemainingDesktopHandles);
+                    return retVal.GetResultOrThrow();
                 }
             } finally {
                 scRequiredDialog.Close();
             }
         }
-        
+
+        private static IEnumerable<IntPtr> GetAndResetRemainingHandles() {
+            ISet<IntPtr> currentRemainingHandles;
+            var nextRemainingHandles = new HashSet<IntPtr>();
+            
+            do {
+                currentRemainingHandles = remainingDesktopHandles;
+
+                // currently this succeeds immediately, as the maxWorker setting effectively serialized things
+                // but: be 100% sure in case of future changes
+            } while(currentRemainingHandles != Interlocked.CompareExchange(ref remainingDesktopHandles,
+                        nextRemainingHandles, currentRemainingHandles));
+
+            return currentRemainingHandles;
+        }
+        private static void AddRemainingHandles(IEnumerable<IntPtr> newRemainingHandles) {
+            ISet<IntPtr> currentRemainingHandles;
+            ISet<IntPtr> nextRemainingHandles;
+            
+            do {
+                currentRemainingHandles = remainingDesktopHandles;
+                nextRemainingHandles = new HashSet<IntPtr>(currentRemainingHandles);
+                foreach (var handle in newRemainingHandles) {
+                    nextRemainingHandles.Add(handle);
+                }
+
+                // currently this succeeds immediately, as the maxWorker setting effectively serialized things
+                // but: be 100% sure in case of future changes
+            } while(currentRemainingHandles != Interlocked.CompareExchange(ref remainingDesktopHandles,
+                nextRemainingHandles, currentRemainingHandles));
+            
+        }
+
+        // Switch to "secure" desktop prior to smart card operations: This is needed for any user interaction to
+        // be visible if "secure" desktop is used.
+        // Note: Some system components, e.g. Win 10 smartcard dialogs, keep running in background for a while after
+        // the dialog has ended. This prevents the secure desktop handle to be closed immediately. An attempt to close
+        // remaining handles is done with every smartcard operation.
+        // As long as desktop handles are not closed, the temporary "secure" desktop created by keepass remains active.
+        // Therefore, if immediate closing fails, the temporary desktop is kept alive longer than needed, e.g. until
+        // the next smartcard operation. This is tolerated. It doesn't affect user experience, but increases resource
+        // consumption.
+        // At latest, all desktop handles are released when the worker process shuts down, that is after the chosen
+        // standby timeout has passed without user operation.
+        private static WorkerResult SetDesktopAndExecute(
+            CancellationToken ct, InvocationRequest.PortableInvocationRequest request, string desktop,
+            IEnumerable<IntPtr> desktopHandlesToClose) {
+
+            var remainingHandles = new List<IntPtr>(desktopHandlesToClose);
+            var currentDesktopHandle = IntPtr.Zero;
+
+            var result = new WorkerResult();
+
+            try {
+                if (desktop != null && desktop != DefaultDesktopName ) {
+                    currentDesktopHandle = NativeForms.GetCurrentThreadDesktop();
+                    var secureDesktopHandle = NativeForms.OpenDesktop(desktop);
+
+                    if (currentDesktopHandle != secureDesktopHandle) {
+                        NativeForms.SetCurrentThreadDesktop(secureDesktopHandle);
+                        remainingHandles.Add(secureDesktopHandle);
+                    } else {
+                        NativeForms.CloseDesktop(secureDesktopHandle);
+                    }
+                }
+
+                try {
+                    var invocationResult = request.ToInvocationRequest().Invoke(ct);
+                    result.SetResult(invocationResult);
+                } catch (Exception e) {
+                    result.AddException(e);
+                }
+            } finally {
+                try {
+                    Process.GetCurrentProcess().CloseMainWindow();
+                } catch { /* try anyway - no need to forward exception */ }
+
+                try {
+                    if (currentDesktopHandle != IntPtr.Zero) {
+                        NativeForms.SetCurrentThreadDesktop(currentDesktopHandle);
+                    }
+                } catch (Exception e) {
+                    result.AddException(e);
+                }
+
+                result.SetRemainingDesktopHandles(remainingHandles.Where(h => {
+                    try {
+                        NativeForms.CloseDesktop(h);
+                        return false;
+                    } catch (Win32Exception e) {
+                        const int errorBusy =  0x000000AA;
+                        const int errorInvalid = 0x00000006;
+
+                        switch (e.NativeErrorCode) {
+                            case errorBusy:
+                                // try again next time
+                                return true;
+                            case errorInvalid:
+                                // desktop has already been disposed (maybe new worker process)
+                                return false;
+                            default:
+                                result.AddException(e);
+                                return true;
+                        }
+                    } catch (Exception e) {
+                        result.AddException(e);
+                        return true;
+                    }
+                }).ToList());
+            }
+
+            return result;
+        }
+                
         #endregion
     }
 }
--- SmartcardEncryptedKeyFile/UI/Windows/NativeForms.cs
+++ SmartcardEncryptedKeyFile/UI/Windows/NativeForms.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
@@ -14,6 +15,22 @@ namespace Episource.KeePass.EKF.UI.Windows {
             GWL_EXSTYLE = -20,
             GWL_HWNDPARENT = -8,
             GWL_STYLE = -16
+        }
+
+        /// <summary>
+        /// Subset of: https://docs.microsoft.com/de-de/windows/desktop/winstation/desktop-security-and-access-rights
+        /// </summary>
+        [Flags]
+        public enum DesktopFlags : uint {
+            CreateMenu = 0x0004,
+            CreateWindow = 0x0002,
+            Enumerate = 0x0040,
+            HookControl = 0x0008,
+            JournalPlayback = 0x0020,
+            JournalRecord = 0x0010,
+            ReadObjects = 0x0001,
+            SwitchDesktop = 0x0100,
+            WriteObjects = 0x0080
         }
         
         [DllImport("user32.dll", EntryPoint = "GetActiveWindow", CharSet = CharSet.Auto)]
@@ -30,6 +47,39 @@ namespace Episource.KeePass.EKF.UI.Windows {
         
         [DllImport("user32.dll", SetLastError = true)]
         private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        
+        [DllImport("User32.dll", EntryPoint = "OpenDesktop", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr OpenDesktopNative(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpszDesktop, int dwFlags,
+            [MarshalAs(UnmanagedType.Bool)] bool fInherit,
+            [MarshalAs(UnmanagedType.U4)] DesktopFlags dwDesiredAccess
+        );
+
+        [DllImport("User32.dll", EntryPoint = "OpenInputDesktop", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr OpenInputDesktopNative(
+            int dwFlags, [MarshalAs(UnmanagedType.Bool)] bool fInherit,
+            [MarshalAs(UnmanagedType.U4)] DesktopFlags dwDesiredAccess
+        );
+
+        [DllImport("User32.dll", EntryPoint = "CloseDesktop", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseDesktopNative(IntPtr hDesktop);
+        
+        [DllImport("User32.dll", EntryPoint = "GetThreadDesktop", SetLastError = true)]
+        private static extern IntPtr GetThreadDesktopNative(uint dwThreadId);
+        
+        [DllImport("User32.dll", EntryPoint = "SetThreadDesktop", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetThreadDesktopNative(IntPtr hDesktop);
+        
+        [DllImport("Kernel32.dll")]
+        private static extern uint GetCurrentThreadId();
+
+        [DllImport("User32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetUserObjectInformation(
+            IntPtr hObj, int nIndex, IntPtr pvInfo, uint nLength, ref uint lpnLengthNeeded
+        );
 
         private static IntPtr GetWindowLongImpl(IntPtr hWnd, WindowParamIndex nIndex) {
             var result = IntPtr.Size == 4 
@@ -82,6 +132,100 @@ namespace Episource.KeePass.EKF.UI.Windows {
             }
 
             return Control.FromHandle(activeWinHandle) as Form;
+        }
+
+        public static IntPtr GetThreadDesktop(uint threadId) {
+            var desktop = GetThreadDesktopNative(threadId);
+            if (desktop == IntPtr.Zero) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return desktop;
+        }
+
+        public static IntPtr GetThreadDesktop(ProcessThread thread) {
+            return GetThreadDesktop((uint)thread.Id);
+        }
+
+        public static IntPtr GetCurrentThreadDesktop() {
+            var currentThread = GetCurrentThreadId();
+            return GetThreadDesktop(currentThread);
+        }
+
+        public static string GetCurrentThreadDesktopName() {
+            return GetDesktopName(GetCurrentThreadDesktop());
+        }
+
+        public static void SetCurrentThreadDesktop(IntPtr desktop) {
+            if (desktop == IntPtr.Zero) {
+                throw new ArgumentException("desktop == IntPtr.Zero", "desktop");
+            }
+
+            if (!SetThreadDesktopNative(desktop)) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        public static string GetDesktopName(IntPtr desktop) {
+            if (desktop == IntPtr.Zero) {
+                throw new ArgumentException("desktop == IntPtr.Zero", "desktop");
+            }
+            
+            // https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/18d8fbe8-a967-4f1c-ae50-99ca8e491d2d
+            const int errorInsufficientBuffer = 0x7a;
+            
+            // UOI_NAME: https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-getuserobjectinformationw
+            const int uoiNameIndex = 2;
+
+            var bufferSize = 0u;
+            GetUserObjectInformation(desktop, uoiNameIndex, IntPtr.Zero, bufferSize, ref bufferSize);
+            var error = Marshal.GetLastWin32Error();
+            if (error != errorInsufficientBuffer) {
+                throw new Win32Exception(error);
+            }
+
+            var strBuf = IntPtr.Zero;
+            try {
+                strBuf = Marshal.AllocHGlobal((int) bufferSize);
+                if (!GetUserObjectInformation(desktop, uoiNameIndex, strBuf, bufferSize, ref bufferSize)) {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+
+                return Marshal.PtrToStringUni(strBuf);
+            } finally {
+                Marshal.FreeHGlobal(strBuf);
+            }
+
+        }
+
+        public static IntPtr OpenDesktop(string desktopName, bool inherit = false,
+            DesktopFlags flags = DesktopFlags.CreateMenu   | DesktopFlags.CreateWindow | DesktopFlags.ReadObjects |
+                                 DesktopFlags.WriteObjects | DesktopFlags.SwitchDesktop
+        ) {
+            var result = OpenDesktopNative(desktopName, 0, inherit, flags);
+            if (result == IntPtr.Zero) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return result;
+        }
+
+        public static IntPtr OpenInputDesktop(bool inherit = false,
+            DesktopFlags flags = DesktopFlags.CreateMenu   | DesktopFlags.CreateWindow | DesktopFlags.ReadObjects |
+                                 DesktopFlags.WriteObjects | DesktopFlags.SwitchDesktop
+        ) {
+            var result = OpenInputDesktopNative(0, inherit, flags);
+            if (result == IntPtr.Zero) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return result;
+        }
+
+        public static void CloseDesktop(IntPtr desktop) {
+            if (!CloseDesktopNative(desktop)) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
         }
     }
 }
